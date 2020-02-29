bb62da3c5002de2fb8a36e98ef7f224581038ee1
Pedro Arredondo <p3dvvr@gmail.com>
2020-02-29 15:13:00 -0600

    Revert "importing project"
    This reverts commit 044edf61f983926b3e4a332947647894c78f125e.

--- To Do List/Items.cs
+++ To Do List/Items.cs
@@ -1,15 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace To_Do_List
 {
-    //the objects made from this class will be serialized and saved
     [Serializable]
-    //this class of items are what are placed into your to do lists
     public class Items
     {
-        //the string is what the user inputs into the list
         public string Goal;
-        //and the boolean is what indicates whether it is complete or not
         public bool Finished;
         public Items(string aGoal, bool notFinished)
         {
